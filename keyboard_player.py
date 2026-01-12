28c7cf78d91fe4b6c34139f8014703c194a30a41
YukioKoito <25551010+OugiFormula@users.noreply.github.com>
2026-01-12 22:56:00 +0200

    22 keys for piano

--- keyboard_player.py
+++ keyboard_player.py
@@ -3,8 +3,37 @@ import keyboard
 import threading
 import mido
 
-# 15-key game mapping
-NOTE_TO_KEY = {
+# Key mappings for 15-key and 22-key modes
+NOTE_TO_KEY_15 = {
+    ("C", 4): "a",
+    ("D", 4): "s",
+    ("E", 4): "d",
+    ("F", 4): "f",
+    ("G", 4): "g",
+    ("A", 4): "h",
+    ("B", 4): "j",
+
+    ("C", 5): "q",
+    ("D", 5): "w",
+    ("E", 5): "e",
+    ("F", 5): "r",
+    ("G", 5): "t",
+    ("A", 5): "y",
+    ("B", 5): "u",
+
+    ("C", 6): "i",
+}
+
+NOTE_TO_KEY_22 = {
+    # Low octave for piano
+    ("C", 3): "z",
+    ("D", 3): "x",
+    ("E", 3): "c",
+    ("F", 3): "v",
+    ("G", 3): "b",
+    ("A", 3): "n",
+    ("B", 3): "m",
+
     ("C", 4): "a",
     ("D", 4): "s",
     ("E", 4): "d",
@@ -29,20 +58,26 @@ MAX_OCTAVE = 6
 
 # KeyboardPlayer: plays MIDI files
 class KeyboardPlayer:
-    def __init__(self):
+    def __init__(self, layout="15"):
         self.stop_flag = False
         self.thread = None
+        self.set_layout(layout)
+
+    def set_layout(self, layout):
+        self.layout = layout
+        self.note_map = NOTE_TO_KEY_22 if layout == "22" else NOTE_TO_KEY_15
 
     def stop(self):
         self.stop_flag = True
 
     def get_playable_key(self, note):
         name, octave = note
-        while (name, octave) not in NOTE_TO_KEY and octave < MAX_OCTAVE:
+        # Adjust octave to match playable keys
+        while (name, octave) not in self.note_map and octave < MAX_OCTAVE:
             octave += 1
-        while (name, octave) not in NOTE_TO_KEY and octave > MIN_OCTAVE:
+        while (name, octave) not in self.note_map and octave > MIN_OCTAVE:
             octave -= 1
-        return NOTE_TO_KEY.get((name, octave))
+        return self.note_map.get((name, octave))
 
     def play(self, events, speed=1.0, on_key_press=None):
         self.stop_flag = False
@@ -72,12 +107,8 @@ class KeyboardPlayer:
 
 # MidiInputPlayer: live MIDI keyboard
 class MidiInputPlayer:
-    def __init__(self, note_to_key=NOTE_TO_KEY, on_key_press=None, transpose=0, tap_length=0.03):
-        """
-        transpose: number of MIDI notes to shift (positive or negative)
-        tap_length: duration of key press in seconds
-        """
-        self.note_to_key = note_to_key
+    def __init__(self, note_to_key=None, on_key_press=None, transpose=0, tap_length=0.03):
+        self.note_to_key = note_to_key if note_to_key else NOTE_TO_KEY_15
         self.on_key_press = on_key_press
         self.transpose = transpose
         self.tap_length = tap_length
@@ -120,9 +151,7 @@ class MidiInputPlayer:
     def get_playable_key(self, midi_note):
         NOTE_NAMES = ["C", "C#", "D", "D#", "E", "F",
                       "F#", "G", "G#", "A", "A#", "B"]
-
         midi_note += self.transpose
-
         name = NOTE_NAMES[midi_note % 12]
         octave = midi_note // 12 - 1
 
@@ -130,5 +159,4 @@ class MidiInputPlayer:
             octave += 1
         while (name, octave) not in self.note_to_key and octave > MIN_OCTAVE:
             octave -= 1
-
         return self.note_to_key.get((name, octave))
